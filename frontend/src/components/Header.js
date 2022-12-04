55a1e2cdd43b3c1971afc72cf9119129093ec492
Adeyemi <yemiklein77@gmail.com>
2022-12-04 18:39:45 +0100

    updates

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -14,16 +14,16 @@ const Header = () => {
     return (
         <div className="header">
             <div className="header-flex">
-                <h1>EventsPlus</h1>
+                <h1>EventPlus</h1>
                 {!token && !loading && error ?
                     (<div className="btn-flex">
                         <Link to="/login"><button className="primary-btn">Log In</button></Link>
                         <Link to="/register"><button className="primary-btn">Register</button></Link>
                     </div>)
                     : loading && !error ? <p>Loading...</p>
-                    : !loading && data !== null ?(
+                    : !loading && data !== null ? (
                         <div className="user-details-flex">
-                            <p>{data.user.email.split('@')[0]}</p>
+                            <p>{data.user.email.toUpperCase().split('@')[0]}</p>
                             <button className="primary-btn" onClick={logout}>Log Out</button>
                         </div>
                     ) : <p>Not Logged In!</p>
